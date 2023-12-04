bd49ab686cede504cb01084d6b4efe44ee9ae3f5
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-12-04 09:57:32 +0200

    day-1 | difference between fibonacci and factorial | TDD

---
+++ day-1/difference-between-fibonacci-and-factorial/calculations.py
@@ -0,0 +1,19 @@
+class FactorialCalculation:
+    def get_factorial_item(self, item_index: int) -> int:
+        return 1 if item_index == 0 else item_index * self.get_factorial_item(item_index - 1)
+
+
+class FibonacciCalculation:
+    def get_fibonacci_item(self, item_index: int) -> int:
+        is_except_initial_case = item_index == 0 or item_index == 1 or item_index == 2
+
+        return 1 if is_except_initial_case else \
+            self.get_fibonacci_item(item_index - 1) + self.get_fibonacci_item(item_index - 2)
+
+
+class CoreCalculations:
+    __factorial = FactorialCalculation()
+    __fibonacci = FibonacciCalculation()
+
+    def get_core_elements_difference(self, item_index: int) -> int:
+        return self.__factorial.get_factorial_item(item_index) - self.__fibonacci.get_fibonacci_item(item_index)
---
+++ day-1/difference-between-fibonacci-and-factorial/calculations_tests.py
@@ -0,0 +1,29 @@
+import unittest
+from calculations import *
+
+
+class MathCalculationsTestCases(unittest.TestCase):
+    __factorial = FactorialCalculation()
+    __fibonacci = FibonacciCalculation()
+    __calculations = CoreCalculations()
+
+    def test_factorial(self):
+        self.assertEqual(self.__factorial.get_factorial_item(3), 6)
+        self.assertEqual(self.__factorial.get_factorial_item(7), 5040)
+        self.assertEqual(self.__factorial.get_factorial_item(5), 120)
+        self.assertEqual(self.__factorial.get_factorial_item(0), 1)
+
+    def test_fibonacci(self):
+        self.assertEqual(self.__fibonacci.get_fibonacci_item(8), 21)
+        self.assertEqual(self.__fibonacci.get_fibonacci_item(12), 144)
+        self.assertEqual(self.__fibonacci.get_fibonacci_item(2), 1)
+        self.assertEqual(self.__fibonacci.get_fibonacci_item(5), 5)
+
+    def test_core_calculations(self):
+        self.assertEqual(self.__calculations.get_core_elements_difference(5), 115)
+        self.assertEqual(self.__calculations.get_core_elements_difference(8), 40299)
+        self.assertEqual(self.__calculations.get_core_elements_difference(0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
